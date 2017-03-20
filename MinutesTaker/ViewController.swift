bd66a1c3512733a411ae863b8648e671450a2ddc
faisalkhalid <k112023@nu.edu.pk>
2017-03-20 11:37:41 +0400

    Initial Commit

---
+++ MinutesTaker/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MinutesTaker
+//
+//  Created by faisal khalid on 3/20/17.
+//  Copyright © 2017 Sharjah Cooperative Society. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
